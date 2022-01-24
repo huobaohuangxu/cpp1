d4db045d5feadfdcd0ec9931844bb5fe60b41c06
huobaohuangxu <huobaohuangxu@gmail.com>
2022-01-24 14:58:15 +0800

    添加项目文件。

---
+++ cpp1/test.c
@@ -0,0 +1,57 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main() {
+//	printf("hello world\n");
+//	//printf("%d\n", 100);//输出整形
+//	//printf("%d\n", sizeof(char));
+//	////sizeof计算类型或变量所占空间大小
+//	return 0;
+//}
+
+//int main()
+//{
+//	int age = 20;
+//	double weight = 75.3;
+//
+//	age = age + 1;
+//	weight = weight - 10;
+//
+//	printf("%d\n", age);//输出整数
+//	printf("%lf\n", weight);//输出双精度浮点数
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int sum = 0;
+//	//输入函数
+//	//分别对应输入
+//	scanf("%d %d", &a, &b);
+//	
+//	sum = a + b;
+//	printf("sum=%d\n", sum);
+//	return 0;
+//}
+
+////简单跨文件声明变量
+//extern int g_val;
+//int main() {
+//	printf("%d", g_val);
+//	return 0;
+//}
+
+////const修饰常变量
+//const int num = 10;//num仍为变量
+////num是常变量，具有常属性（不能被改变）
+//int main()
+//{
+////int arr[num] = { 0 };
+////变量不可直接用于数组
+//	//num = 20;
+//	printf("num = %d\n",  num);
+//	return 0;
+//}
+
